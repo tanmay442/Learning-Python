26dfabd9eebbc8e49fd555ccbf11a6595a60c9dc
Tanmay Goel <108540721+tanmay442@users.noreply.github.com>
2024-02-25 14:27:37 +0530

    Update #Power Rename.py

--- #Power Rename.py
+++ #Power Rename.py
@@ -1,17 +1,25 @@
 import os
 import PySimpleGUI as sg
 
-sg.theme('DarkAmber')  # Add a touch of color
+sg.theme('DarkAmber')  
 
-def ren():
+def ren():     
     files_list=(os.listdir(f"{folder}"))
     files_list=sorted(files_list)
-    ln=len(files_list)
+    ln=len(files_list) 
+
     n=0
     for i in range(ln):
         n+=1
+
+        a=files_list[i][::-1]            
+        b=a.find(".")        
+        extension=a[:b+1]
+        extension=extension[::-1]
+        
         os.rename(folder+"/"+files_list[i],folder+"/"+f"{n}%%##$$"+extension)
-        files_list[i]=f"{n}%%##$$"+extension
+        files_list[i]=f"{n}%%##$$"+extension 
+           
     n=0
     for j in files_list:
         n+=1
@@ -19,12 +27,12 @@ def ren():
 
 layout = [
     [sg.Text("Choose the Directory to implement the sorter", text_color='white'), sg.Input(), sg.FolderBrowse(key='-FOLDER-', button_color=('white', 'blue'))],
-    [[sg.Text("Enter the extension with dot")],
-          [sg.Input(key='-IN-', text_color='white', background_color='dark blue')]],
+    
     [[sg.Text("Enter the Common Title")],
           [sg.Input(key='-OP-', text_color='white', background_color='dark blue')]],
+
     [sg.Button("Submit", button_color=('white', 'green')), sg.Button("Cancel", button_color=('white', 'red'))],
-    [sg.Text("")]
+    
 ]
 
 window = sg.Window("Choose the Directory to Rename the files", layout, text_justification='center')
@@ -35,8 +43,7 @@ while True:
         break
     if event == "Submit":
         folder = values['-FOLDER-']
-        ctile = values['-OP-']
-        extension = values['-IN-']
+        ctile = values['-OP-']        
         print("Selected",folder)
         ren()
         break
